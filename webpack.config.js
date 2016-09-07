9131c939401d9922fb0831b2ef883eff36931bc1
mikqmas <samkim86@gmail.com>
2016-09-07 11:57:32 -0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+// webpack.config.js
+module.exports = {
+  entry: "./entry.jsx",
+  output: {
+    path: "./javascripts",
+    filename: "bundle.js",
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
